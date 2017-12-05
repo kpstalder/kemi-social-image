b49ac75e92bcb84aa23de7223e3b2394e7625ee7
Kevin Stalder <kpstalder.dev@gmail.com>
2017-12-04 21:08:34 -0500

    fixing logic for socail image

--- kemi-social-image.php
+++ kemi-social-image.php
@@ -20,14 +20,20 @@ require_once( 'admin/post-meta-box.php' );
 
 add_filter('wpseo_opengraph_image','add_custom_social_image' , 10, 2 );
 add_filter('wpseo_twitter_image','add_custom_social_image' , 10, 2 );
-function add_custom_social_image(){
+ function add_custom_social_image(){
   //Logic Order
     // 1st - Social Image on post
     // 2nd - Featured Image on post
     // 3rd - Default Image from theme settings
-  $options = get_option( 'kemi_social_images_options' );
-  $social_image = wp_get_attachment_url(  $options['kemi_social_images'] );
-
+  global $post;
+  if(get_post_meta( $post->ID, '_listing_image_id', true )){
+    $social_image = wp_get_attachment_image_url( get_post_meta( $post->ID, '_listing_image_id', true ), 'full' );
+  } else if ( has_post_thumbnail($post->ID)){
+      $social_image = get_the_post_thumbnail_url($post->ID);
+  }else{
+    $options = get_option( 'kemi_social_images_options' );
+    $social_image = wp_get_attachment_url(  $options['kemi_social_images'] );
+  }
   return $social_image;
 
-}
+ }
