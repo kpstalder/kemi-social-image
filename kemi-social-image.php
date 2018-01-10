5c0682afe944e7e2bc6a2d1e4b7289b4b8ce38fc
Kevin Stalder <kpstalder.dev@gmail.com>
2018-01-09 21:19:56 -0500

    finalizing code to add social image

--- kemi-social-image.php
+++ kemi-social-image.php
@@ -18,6 +18,21 @@ Domain Path:  /languages
 require_once( 'admin/admin-page.php' );
 require_once( 'admin/post-meta-box.php' );
 
+
+function custom_social_image(){
+  global $post;
+  if(get_post_meta( $post->ID, '_listing_image_id', true )){
+    $social_image = wp_get_attachment_image_url( get_post_meta( $post->ID, '_listing_image_id', true ), 'full' );
+  } else if ( has_post_thumbnail($post->ID)){
+      $social_image = get_the_post_thumbnail_url($post->ID);
+  }else{
+    $options = get_option( 'kemi_social_images_options' );
+    $social_image = wp_get_attachment_url(  $options['kemi_social_images'] );
+  }
+  return $social_image;
+}
+
+
 add_action( 'wpseo_add_opengraph_images', 'add_custom_social_image', 10);
 // add_filter('wpseo_twitter_image','testing_123' , 10, 2 );
 
@@ -26,59 +41,20 @@ function add_custom_social_image($object){
     // 1st - Social Image on post
     // 2nd - Featured Image on post
     // 3rd - Default Image from theme settings
-  global $post;
-  if(get_post_meta( $post->ID, '_listing_image_id', true )){
-    $social_image = wp_get_attachment_image_url( get_post_meta( $post->ID, '_listing_image_id', true ), 'full' );
-    $object->add_image($social_image);
-  } else if ( has_post_thumbnail($post->ID)){
-  } else {
-    $options = get_option( 'kemi_social_images_options' );
-    $social_image = wp_get_attachment_url(  $options['kemi_social_images'] );
-    $object->add_image($social_image);
+  $social_image = custom_social_image();
+  $object->add_image($social_image);
+
+}
+
+
+ add_filter('wpseo_opengraph_image','filter_custom_social_image' , 10, 2 );
+ add_filter('wpseo_twitter_image','filter_custom_social_image' , 10, 2 );
+  function filter_custom_social_image(){
+   //Logic Order
+     // 1st - Social Image on post
+     // 2nd - Featured Image on post
+     // 3rd - Default Image from theme settings
+   $social_image = custom_social_image();
+   return $social_image;
+
   }
-
-
- }
- function my_own_og_function() {
-     /*
-      Put the logic to determine which image to display here (set the $my_image_url variable)
-     */
-     global $post;
-     do {
-       if ( has_post_thumbnail($post->ID) ){
-           continue;
-       } else if(get_post_meta( $post->ID, '_listing_image_id', true )){
-         $social_image = wp_get_attachment_image_url( get_post_meta( $post->ID, '_listing_image_id', true ), 'full' );
-         $GLOBALS['wpseo_og']->image_output( $social_image ); // This will echo out the og tag in line with other WPSEO og tags
-       }else{
-         $options = get_option( 'kemi_social_images_options' );
-         $social_image = wp_get_attachment_url(  $options['kemi_social_images'] );
-         $GLOBALS['wpseo_og']->image_output( $social_image ); // This will echo out the og tag in line with other WPSEO og tags
-       }
-     } while (0);
-
-//     $GLOBALS['wpseo_og']->options->og_default_image = $social_image."2" ;
-     //$GLOBALS['wpseo_tw']->image_output( $social_image ); // This will echo out the og tag in line with
-
- }
- //add_action( 'wpseo_opengraph', 'my_own_og_function', 29 );
- function my_own_twitter_function() {
-     /*
-      Put the logic to determine which image to display here (set the $my_image_url variable)
-     */
-     global $post;
-     do {
-       if ( has_post_thumbnail($post->ID)){
-           break;
-       } else if(get_post_meta( $post->ID, '_listing_image_id', true )){
-         $social_image = wp_get_attachment_image_url( get_post_meta( $post->ID, '_listing_image_id', true ), 'full' );
-          echo '<meta name="twitter:image" content="'.$social_image.'" />';
-       } else{
-         $options = get_option( 'kemi_social_images_options' );
-         $social_image = wp_get_attachment_url(  $options['kemi_social_images'] );
-          echo '<meta name="twitter:image" content="'.$social_image.'" />';
-       }
-     } while (0);
-
- }
-//  add_action( 'wpseo_twitter', 'my_own_twitter_function', 29 );
